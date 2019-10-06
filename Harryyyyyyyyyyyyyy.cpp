11a85122997175b4112e0cb66700a5b3380e2de0
Riadrox <53078985+riadrox@users.noreply.github.com>
2019-10-06 09:25:29 +0600

    Create Harryyyyyyyyyyyyyy.cpp

---
+++ Harryyyyyyyyyyyyyy.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n,i,k,j;
+	cin>>k;
+	for(j=0;j<k;j++)
+	{
+	cin>>n;
+		cout<<"Case "<<j+1<<": Harr";
+	for(i=0;i<n;i++)
+		cout<<"y";
+	cout<<"! Khelbe Hogwarts, Jitbe Hogwarts!\n";
+	}
+	return 0;
+}
